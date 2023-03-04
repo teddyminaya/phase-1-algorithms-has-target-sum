975a4f2d7a238d15c767e2cb78913db438ec34d9
Teddy Minaya <teddyminaya@gmail.com>
2023-03-03 19:31:01 -0500

    commit tree 3f8a8c9aec0f07c95b1b27f13e9895912e0e5e9b

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Iterate through the array
+  for (let i = 0; i < array.length; i++) {
+    const num1 = array[i];
+    // Check if the target minus the current number is in the rest of the array
+    for (let j = i + 1; j < array.length; j++) {
+      const num2 = array[j];
+      if (num1 + num2 === target) {
+        // If we find a pair of numbers that add up to the target, return true
+        return true;
+      }
+    }
+  }
+  // If we go through the whole array and haven't found a pair that adds up to the target, return false
+  return false;
 }
 
+
 /* 
-  Write the Big O time complexity of your function here
+  0(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  Iterate through the array. Check if the target minus the current number is in the rest of the array. If we find a pair of numbers that add up to the target, return true
 */
 
 /*
-  Add written explanation of your solution here
+  This code uses two nested loops to iterate through the array and compare each pair of numbers. For each number in the array, it checks if the target minus that number is in the rest of the array. If it finds a pair of numbers that add up to the target, it returns true. If it goes through the whole array without finding a pair that adds up to the target, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
